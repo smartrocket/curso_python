3ca1aab08710494729494d0e9be377d561ead19a
Dayane Pabla <dayanepabla@gmail.com>
2014-07-07 15:48:10 -0300

    Organização dos aquivos

--- level2/entrada.py
+++
@@ -1,4 +0,0 @@
-#-*- encoding: utf-8 -*-
-nome = raw_input('Informe o seu nome: ')
-idade = input('Informe a sua idade: ')
-print ('Olá %s, você tem %i anos.' %(nome, idade))
--- level2/tipo_logico.py
+++
@@ -1,4 +0,0 @@
-idade = 15
-maior_idade = 16
-eh_eleitor = idade >= maior_idade
-print (eh_eleitor)
