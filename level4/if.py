f02f20473b67132d84a022db4c709f9be456404d
Dayane Pabla <dayanepabla@gmail.com>
2014-07-11 00:32:48 -0300

    Início do level4

---
+++ level4/if.py
@@ -0,0 +1,7 @@
+#-*- encoding: utf-8 -*-
+x = input('Primeiro número: ')
+y = input('Segundo número: ')
+if x > y:
+    print('O primeiro número é o maior!')
+if y > x:
+    print('O segundo número é o maior!')
