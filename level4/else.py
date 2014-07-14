f51bd21e5ea6a5a2cbfbb3b57ee80326c9a07bc0
Dayane Pabla <dayanepabla@gmail.com>
2014-07-14 01:15:15 -0300

    Adição de exemplo do uso da condição else

---
+++ level4/else.py
@@ -0,0 +1,7 @@
+#-*- encoding: utf-8 -*-
+x = input('Primeiro número: ')
+y = input('Segundo número: ')
+if x > y:
+    print('O primeiro número é o maior!')
+else:
+    print('O segundo número é o maior!')
