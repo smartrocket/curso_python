65ebe4500b8dd7887d2a60988a1fe8c9fca16ea8
Dayane Pabla <dayanepabla@gmail.com>
2014-08-11 22:04:16 -0300

    Modificação no exemplo

--- level6/apres_numeros.py
+++ level6/apres_numeros.py
@@ -1,13 +1,12 @@
 #-*- encoding: utf-8 -*-
-numeros = [0] * 5
-
-for x in range(5):
-    numeros[x] = input('Número %d: ' %(x + 1))
-
+numeros = [0,0,0,0,0]
+z = 0
+while z < 5:
+    numeros[z] = input('Número %d:' %(z+1))
+    z+=1
 while True:
     print('Digite 0 para sair')
     escolhido = input('Posição que você deseja imprimir: ')
-    if escolhido is 0:
+    if escolhido == 0:
         break
-
     print('Você escolheu o número: %d' % (numeros[escolhido - 1]))
