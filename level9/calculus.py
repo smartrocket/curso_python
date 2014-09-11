9bb03ec2bc02c930e40cd3272ed67d5debac60f6
Dayane Pabla <dayanepabla@gmail.com>
2014-09-11 00:10:30 -0300

    Exemplo do uso de módulos

---
+++ level9/calculus.py
@@ -0,0 +1,6 @@
+#-*- encoding: utf-8 -*-
+def soma_inteiros(*args):
+    s=0
+    for x in args:
+        s+=x
+    return s
--- level9/soma.py
+++ level9/soma.py
@@ -1,5 +1,7 @@
-def soma(a,b):
-    print(a+b)
-soma(3,4)
-soma(2,8)
-soma(5,7)
+#-*- encoding: utf-8 -*-
+import calculus
+
+l = []
+for i in range(10):
+    l.append(input("Digite um numero: "))
+print "Total", calculus.soma_inteiros(*l)
