fa2e7bde9a7a3d2374665fc3858dd22645e95bfe
Dayane Pabla <dayanepabla@gmail.com>
2014-09-17 16:53:30 -0300

    Exemplo de como obter mais informações sobre o arquivo

--- level10/informacoes.py
+++ level10/informacoes.py
@@ -6,5 +6,5 @@ nome = sys.argv[0]
 print("Nome: %s" % nome)
 print("Tamanho: %d" % os.path.getsize(nome))
 print("Criado: %s" % time.ctime(os.path.getctime(nome)))
-print("Modificado: %s" % time.ctime(os.path.getctime(nome)))
+print("Modificado: %s" % time.ctime(os.path.getmtime(nome)))
 print("Acessado: %s" % time.ctime(os.path.getatime(nome)))
